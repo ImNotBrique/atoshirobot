400afc8cfd57336d617b063e909d0556273c4735
ImNotBrique <52548100+ImNotBrique@users.noreply.github.com>
2019-07-05 22:58:19 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -136,13 +136,12 @@ client.on("message", async message => {
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
       
-      
-   if(command === "annonce") {
-
-  var e = new Discord.RichEmbed()
+          if(message.content === "+annonce"){
+  
+    var e = new Discord.RichEmbed()
   .addField('Annonce :', args.join(' '))
   .setColor('FF0000')
   message.channel.send(e)
 }
   }
-});
+);
