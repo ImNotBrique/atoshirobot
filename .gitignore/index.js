576ec61f18a0300f1e11f794e414b90b114a9ad0
ImNotBrique <52548100+ImNotBrique@users.noreply.github.com>
2019-07-16 19:58:51 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -7,21 +7,21 @@ bot.login(process.env.TOKEN)
 bot.on('ready', function() { 
  console.log('Je suis connecté :D') 
 
-    bot.user.setActivity(`ma bite`, { type: 'WATCHING'});
+    bot.user.setActivity(`Prefix : $ | By ImNotBreak`, { type: 'WATCHING'});
     bot.user.setStatus('dnd')
   });
   
   bot.on("guildCreate", guild => {
     // This event triggers when the bot joins a guild.
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    bot.user.setActivity(`ma bite`, { type: 'WATCHING'});
+    bot.user.setActivity(`Prefix : $ | By ImNotBreak`, { type: 'WATCHING'});
     bot.user.setStatus('dnd')
   });
   
   bot.on("guildDelete", guild => {
     // this event triggers when the bot is removed from a guild.
     console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-    bot.user.setActivity(`ma bite`, { type: 'WATCHING'});
+    bot.user.setActivity(`Prefix : $ | By ImNotBreak`, { type: 'WATCHING'});
     bot.user.setStatus('dnd')
 });
 
